219e47dc5091553c132f019178593cd53f0a394f
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2023-01-19 20:45:02 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (75.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Find cumulative expected sum: n*(n+1)/2
-        // iterate over nums and subtract each num from sum
-        int n = nums.length;
-        int sum = (int) n*(n+1)/2;
-        for (int i = 0; i < n; i++)
-            sum -= nums[i];
-        return sum;
+        int a = 0;
+        int b = 0;
+        for (int i=0; i<nums.length; i++){
+            a ^= i;
+            b ^= nums[i];
+        }
+        a ^= nums.length;
+        // System.out.println(a+" , "+b+" , "+(a^b));
+        return a^b;
     }
 }
