05d6d6ed3c2131aa0e87753f0cd4b78da89ab13d
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2023-01-27 15:23:49 +0530

    Time: 233 ms (16.47%), Space: 44.3 MB (35.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,22 +2,12 @@ class Solution {
     Map<Character,Integer> tMap = new HashMap<>();
     
     public boolean isValidWindow(Map<Character,Integer> windowMap){
-        // checks if all counts of T are subsumed by W
         for (char key : this.tMap.keySet()){
             if (windowMap.getOrDefault(key, 0) < this.tMap.get(key))
                 return false;
         }
         return true;
     }
-    
-    public void print(Map<Character,Integer> map){
-        System.out.println("Hmap:");
-        for (char key : map.keySet()){
-            System.out.print(key+":"+map.get(key)+" ");
-        }
-        System.out.println("");
-    }
-    
     
     public String minWindow(String s, String t) {
         if (t.length() > s.length())
@@ -30,7 +20,6 @@ class Solution {
         
         for (char c : t.toCharArray())
             this.tMap.put(c, this.tMap.getOrDefault(c,0) + 1);
-        // print(this.tMap);
         
         int i=0, j=0;
         char cR, cL;
@@ -53,8 +42,6 @@ class Solution {
                     wMap.put(cR, wMap.getOrDefault(cR, 0) + 1);
                 }
                 if (isValidWindow(wMap)){
-                    // System.out.println(s.substring(i,j+1));
-                    // print(wMap);
                     if (minLen > j-i+1){
                         minL = i;
                         minR = j;
@@ -63,7 +50,6 @@ class Solution {
                     cL = s.charAt(i);
                     wMap.put(cL, wMap.getOrDefault(cL, 0) - 1);
                     i = q.poll();
-                    
                     prevJ = j--;
                 }
             }
@@ -75,57 +61,3 @@ class Solution {
         return s.substring(minL, Math.min(minR+1, s.length()));
     }
 }
-/*
-"ab"
-"A"
-"ab"
-"a"
-"MYNAMEISNOTABCDVIKRANTISAWESOMEITISVARUNTANGIBLEINFORMATION"
-"VIKRANTISAWESOME"
-"ZADOBECODEBANC"
-"ABC"
-"ADOBECODEBANC"
-"ABC"
-"a"
-"a"
-"a"
-"aa"
-"ab"
-"A"
-"ADOBECODEBANC"
-"ABC"
-"a"
-"a"
-"a"
-"z"
-"a"
-"A"
-"a"
-"aa"
-"ab"
-"a"
-"aa"
-"aa"
-"BBB"
-"BBB"
-"abcd"
-"abcd"
-"acbbaca"
-"aba"
-"abcdefghi"
-"def"
-"abcdefghi"
-"fd"
-"abcdefghi"
-"geb"
-"abcdefghi"
-"gzb"
-"abcdefghi"
-"geeb"
-"aaaaxyzczyxbbb"
-"bbaac"
-"cabwefgewcwaefgcf"
-"cae"
-"obczoaooboaocooaobozcoboaoboc"
-"cbza"
-*/
