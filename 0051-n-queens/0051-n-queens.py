5868c7e9809b77c987481173eba7518a407a8e75
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2023-03-02 16:24:12 -0500

    Time: 103 ms (36.35%), Space: 14.5 MB (36.80%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,15 +2,15 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
         
-        def is_on_row_col(pos1, visited_pos):
-            for pos2 in visited_pos:
-                if (pos1[0] == pos2[0]) or (pos1[1] == pos2[1]):
+        def is_on_row_col(r1, c1, visited_pos):
+            for r2, c2 in enumerate(visited_pos):
+                if r1 == r2 or c1 == c2:
                     return True
             return False
 
-        def is_on_diagonal(pos1, visited_pos):
-            for pos2 in visited_pos:
-                if abs(pos2[0]-pos1[0]) == abs(pos2[1]-pos1[1]):
+        def is_on_diagonal(r1, c1, visited_pos):
+            for r2, c2 in enumerate(visited_pos):
+                if abs(r1-r2) == abs(c1-c2):
                     return True
             return False
 
@@ -29,17 +29,17 @@ class Solution:
                 return
             
             for j in range(len(board[0])):
-                if is_on_row_col([r, j], visited_pos) or is_on_diagonal([r, j], visited_pos):
+                if is_on_row_col(r, j, visited_pos) or is_on_diagonal(r, j, visited_pos):
                     continue
-                visited_pos.add((r,j))
+                visited_pos.append(j)
                 board[r][j] = 'Q'
                 dfs(visited_pos, board, r+1)
                 board[r][j] = '.'
-                visited_pos.remove((r,j))
+                _ = visited_pos.pop()
                 
 
         dfs(
-            visited_pos=set(), 
+            visited_pos=list(), 
             board=[['.' for _ in range(n)] for _ in range(n)],
             r=0
         )
