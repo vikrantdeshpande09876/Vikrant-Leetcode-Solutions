d1b24fdab0cc109dc9a30f1dbb7a8c0f7525089a
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2023-03-11 09:54:53 -0500

    Time: 0 ms (100.00%), Space: 40 MB (12.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] hmap = new int[46];
     
-    public int dfs(int n, int numWays){
+    public int climbStairs(int n) {
         if (n == 0)
-            return numWays+1;
+            return 1;
         
-        int res = 0;
         if (hmap[n-1] == 0)
-            hmap[n-1] += dfs(n-1, numWays);
-        res += hmap[n-1];
+            hmap[n-1] += climbStairs(n-1);
+
+        hmap[n] += hmap[n-1];
+        
         if (n-1 > 0){
             if (hmap[n-2] == 0)
-                hmap[n-2] += dfs(n-2, numWays);
-            res += hmap[n-2];
+                hmap[n-2] += climbStairs(n-2);
+            hmap[n] += hmap[n-2];
         }
-        hmap[n] = res;
-        return res;
-    }
-    
-    public int climbStairs(int n) {
-        return dfs(n, 0);
+        
+        return hmap[n];
     }
 }
