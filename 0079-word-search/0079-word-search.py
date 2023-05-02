4c2d6dc0bc4c8d894620a28f666ca51f4e9f0854
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2023-05-02 17:29:11 -0400

    Time: 9507 ms (6.81%), Space: 16.3 MB (5.16%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,19 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def dfs(board, word, visited, i, j, k):
+        def dfs(i, j, k):
             m, n = len(board), len(board[0])
-            if i >= m or i < 0 or j >= n or j < 0 or visited[i][j] or word[k]!=board[i][j]:
+            if i >= m or i < 0 or j >= n or j < 0 or word[k]!=board[i][j]:
                 return False
             
             if k == len(word)-1:
                 return True            
 
-            visited[i][j] = True
+            curr = board[i][j]
+            board[i][j] = '#'
             for move in [[0,1], [1,0], [-1,0], [0,-1]]:
-                if dfs(board, word, visited, i+move[0], j+move[1], k+1):
+                if dfs(i+move[0], j+move[1], k+1):
                     return True
-            visited[i][j] = False
+            board[i][j] = curr
             
             return False
             
@@ -21,7 +22,6 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    visited = [[False for _ in board[0]] for _ in board]
-                    if dfs(board, word, visited, i, j, 0):
+                    if dfs(i, j, 0):
                         return True
         return False
